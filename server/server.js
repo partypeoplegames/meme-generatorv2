d466d52cf19e351d1b19f6bbf9fc4f8e6fcfdc89
Sean <meyersean.c@gmail.com>
2019-03-19 13:12:51 -0500

    ADDS express server to start connecting app with API

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+const app = express();
+const PORT = 1337;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static("dist"));
+
+app.listen(PORT, () =>
+  console.log(`meme generator server listening on port: ${PORT}`)
+);
