d3be38dde58137d22e91d4063a0a45084aa80f8f
Sean <meyersean.c@gmail.com>
2019-03-21 13:38:24 -0500

    adds stuff

--- database/textGenerator.js
+++ database/textGenerator.js
@@ -1,25 +1,9 @@
 const faker = require("faker");
 
-const memeArray = [
-  "{{name.firstName}}",
-  "{{address.streetName}} {{hacker.adjective}} {{hacker.verb}}!!!",
-  "{{random.word}} {{hacker.ingverb}} {{random.word}}...",
-  "{{name.prefix}} {{name.firstName}} {{hacker.adjective}}?",
-  "{{name.prefix}} {{name.lastName}} {{hacker.ingverb}} {{name.jobTitle}}.",
-  "{{name.firstName}} Mc'{{hacker.adjective}}son!!!",
-  "{{hacker.phrase}}?!?!?",
-  "Is this {{hacker.noun}}??",
-  "{{company.bs}}, it's the {{company.bsAdjective}}",
-  "{{company.catchPhrase}} -{{name.firstName}} {{hacker.ingverb}} {{name.lastName}}",
-  "{{company.bsAdjective}} {{company.bsNoun}} {{hacker.ingverb}} {{commerce.product}}"
-];
-
 const memeText = (collection, min, max) => {
   const index = Math.floor(Math.random() * (max - min + 1)) + min;
   console.log("INDEX :", index);
   return faker.fake(collection[index]);
 };
-
-console.log(memeText(memeArray, 0, memeArray.length - 1));
 
 module.exports = { memeText };
--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,36 @@ import React, { Component } from "react";
 import Image from "./Image.js";
 import Caption from "./Caption.js";
 import Thumbnails from "./Thumbnails.js";
+import axios from "axios";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      image: "loading",
       message: "Meme Generator"
     };
+
+    this.handleRandomMeme = this.handleRandomMeme.bind(this);
   }
+  componentDidMount() {
+    this.handleRandomMeme();
+  }
+
+  handleRandomMeme() {
+    axios
+      .get("http://localhost:1337/generateMeme")
+      .then(response => console.log(response.data))
+      .catch(console.error);
+  }
+
   render() {
     return (
       <div>
         <p>{this.state.message}</p>
         <Image />
         <Caption />
+        <button onClick={this.handleRandomMeme}>click for random!</button>
         <Thumbnails />
       </div>
     );
